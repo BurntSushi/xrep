758adddacffce88d91daed1c6f2c281c962e8d80
Behnam Esfahbod <behnam@zwnj.org>
2017-07-11 03:32:36 -0600

    [ignore] Expand tests

--- ignore/tests/gitignore_tests.rs
+++ ignore/tests/gitignore_tests.rs
@@ -22,11 +22,29 @@ fn test_gitignore_files_in_root() {
     let gitignore = get_gitignore();
     let m = |path: &str| -> Match<&Glob> { gitignore.matched(Path::new(path), false) };
 
-    assert!(m("ROOT/file_root_1").is_ignore());
-    assert!(m("ROOT/file_root_2").is_ignore());
-    assert!(m("ROOT/file_root_3").is_none());
-    assert!(m("ROOT/file_root_4").is_none());
-    assert!(m("ROOT/file_root_5").is_none());
+    // 0x
+    assert!(m("ROOT/file_root_00").is_ignore());
+    assert!(m("ROOT/file_root_01").is_none());
+    assert!(m("ROOT/file_root_02").is_none());
+    assert!(m("ROOT/file_root_03").is_none());
+
+    // 1x
+    assert!(m("ROOT/file_root_10").is_ignore());
+    assert!(m("ROOT/file_root_11").is_none());
+    assert!(m("ROOT/file_root_12").is_none());
+    assert!(m("ROOT/file_root_13").is_none());
+
+    // 2x
+    assert!(m("ROOT/file_root_20").is_none());
+    assert!(m("ROOT/file_root_21").is_none());
+    assert!(m("ROOT/file_root_22").is_none());
+    assert!(m("ROOT/file_root_23").is_none());
+
+    // 3x
+    assert!(m("ROOT/file_root_30").is_ignore());
+    assert!(m("ROOT/file_root_31").is_none());
+    assert!(m("ROOT/file_root_32").is_none());
+    assert!(m("ROOT/file_root_33").is_none());
 }
 
 
@@ -35,11 +53,29 @@ fn test_gitignore_files_in_deep() {
     let gitignore = get_gitignore();
     let m = |path: &str| -> Match<&Glob> { gitignore.matched(Path::new(path), false) };
 
-    assert!(m("ROOT/file_parent_dir/file_deep_1").is_ignore());
-    assert!(m("ROOT/file_parent_dir/file_deep_2").is_none());
-    assert!(m("ROOT/file_parent_dir/file_deep_3").is_none());
-    assert!(m("ROOT/file_parent_dir/file_deep_4").is_none());
-    assert!(m("ROOT/file_parent_dir/file_deep_5").is_none());
+    // 0x
+    assert!(m("ROOT/parent_dir/file_deep_00").is_ignore());
+    assert!(m("ROOT/parent_dir/file_deep_01").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_02").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_03").is_none());
+
+    // 1x
+    assert!(m("ROOT/parent_dir/file_deep_10").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_11").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_12").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_13").is_none());
+
+    // 2x
+    assert!(m("ROOT/parent_dir/file_deep_20").is_ignore());
+    assert!(m("ROOT/parent_dir/file_deep_21").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_22").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_23").is_none());
+
+    // 3x
+    assert!(m("ROOT/parent_dir/file_deep_30").is_ignore());
+    assert!(m("ROOT/parent_dir/file_deep_31").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_32").is_none());
+    assert!(m("ROOT/parent_dir/file_deep_33").is_none());
 }
 
 
@@ -48,21 +84,156 @@ fn test_gitignore_dirs_in_root() {
     let gitignore = get_gitignore();
     let m = |path: &str| -> Match<&Glob> { gitignore.matched(Path::new(path), true) };
 
-    assert!(m("ROOT/dir_root_1").is_ignore());
-    assert!(m("ROOT/dir_root_2").is_ignore());
-    assert!(m("ROOT/dir_root_3").is_ignore());
+    // 00
 
-    // in git, dirs don't matter, so the following line does not matter
-    assert!(m("ROOT/dir_root_4").is_none());
-    assert!(m("ROOT/dir_root_4/file").is_ignore());
-    // FIXME: `dir_root_4/*` should also ignore all dirs under `dir_root_4`
-    //assert!(m("ROOT/dir_root_4/child_dir/file").is_ignore());
-    assert!(m("ROOT/dir_root_4/child_dir/file").is_none());
+    assert!(m("ROOT/dir_root_00").is_ignore());
 
-    // in git, dirs don't matter, so the following line does not matter
-    assert!(m("ROOT/dir_root_5").is_none());
-    assert!(m("ROOT/dir_root_5/file").is_ignore());
-    assert!(m("ROOT/dir_root_5/child_dir/file").is_ignore());
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_00/file").is_ignore());
+    assert!(m("ROOT/dir_root_00/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_00/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_00/child_dir/file").is_none());
+
+    // 01
+
+    assert!(m("ROOT/dir_root_01").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_01/file").is_ignore());
+    assert!(m("ROOT/dir_root_01/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_01/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_01/child_dir/file").is_none());
+
+    // 02
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/dir_root_02").is_none());
+
+    assert!(m("ROOT/dir_root_02/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_02/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_02/child_dir/file").is_none());
+
+    // 03
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/dir_root_03").is_none());
+    assert!(m("ROOT/dir_root_03/file").is_ignore());
+    assert!(m("ROOT/dir_root_03/child_dir/file").is_ignore());
+
+    // 10
+
+    assert!(m("ROOT/dir_root_10").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_10/file").is_ignore());
+    assert!(m("ROOT/dir_root_10/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_10/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_10/child_dir/file").is_none());
+
+    // 11
+
+    assert!(m("ROOT/dir_root_11").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_11/file").is_ignore());
+    assert!(m("ROOT/dir_root_11/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_11/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_11/child_dir/file").is_none());
+
+    // 12
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/dir_root_12").is_none());
+
+    assert!(m("ROOT/dir_root_12/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_12/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_12/child_dir/file").is_none());
+
+    // 13
+
+    assert!(m("ROOT/dir_root_13").is_none());
+    assert!(m("ROOT/dir_root_13/file").is_ignore());
+    assert!(m("ROOT/dir_root_13/child_dir/file").is_ignore());
+
+    // 20
+
+    assert!(m("ROOT/dir_root_20").is_none());
+    assert!(m("ROOT/dir_root_20/file").is_none());
+    assert!(m("ROOT/dir_root_20/child_dir/file").is_none());
+
+    // 21
+
+    assert!(m("ROOT/dir_root_21").is_none());
+    assert!(m("ROOT/dir_root_21/file").is_none());
+    assert!(m("ROOT/dir_root_21/child_dir/file").is_none());
+
+    // 22
+
+    assert!(m("ROOT/dir_root_22").is_none());
+    assert!(m("ROOT/dir_root_22/file").is_none());
+    assert!(m("ROOT/dir_root_22/child_dir/file").is_none());
+
+    // 23
+
+    assert!(m("ROOT/dir_root_23").is_none());
+    assert!(m("ROOT/dir_root_23/file").is_none());
+    assert!(m("ROOT/dir_root_23/child_dir/file").is_none());
+
+    // 30
+
+    assert!(m("ROOT/dir_root_30").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_30/file").is_ignore());
+    assert!(m("ROOT/dir_root_30/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_30/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_30/child_dir/file").is_none());
+
+    // 31
+
+    assert!(m("ROOT/dir_root_31").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_31/file").is_ignore());
+    assert!(m("ROOT/dir_root_31/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_31/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_31/child_dir/file").is_none());
+
+    // 32
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/dir_root_32").is_none());
+
+    assert!(m("ROOT/dir_root_32/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/dir_root_32/child_dir/file").is_ignore());
+    assert!(m("ROOT/dir_root_32/child_dir/file").is_none());
+
+    // 33
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/dir_root_33").is_none());
+
+    assert!(m("ROOT/dir_root_33/file").is_ignore());
+
+    assert!(m("ROOT/dir_root_33/child_dir/file").is_ignore());
 }
 
 
@@ -71,20 +242,157 @@ fn test_gitignore_dirs_in_deep() {
     let gitignore = get_gitignore();
     let m = |path: &str| -> Match<&Glob> { gitignore.matched(Path::new(path), true) };
 
-    assert!(m("ROOT/dir_parent_dir/dir_deep_1").is_ignore());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_2").is_none());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_3").is_ignore());
+    // 00
 
-    // in git, dirs don't matter, so the following line does not matter
-    assert!(m("ROOT/dir_parent_dir/dir_deep_4").is_none());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_4/file").is_ignore());
-    // FIXME: `dir_parent_dir/dir_deep_4/*` should also ignore all dirs under
-    // `dir_parent_dir/dir_deep_4`
-    //assert!(m("ROOT/dir_parent_dir/dir_deep_4/child_dir/file").is_ignore());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_4/child_dir/file").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_00").is_ignore());
 
-    // in git, dirs don't matter, so the following line does not matter
-    assert!(m("ROOT/dir_parent_dir/dir_deep_5").is_none());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_5/file").is_ignore());
-    assert!(m("ROOT/dir_parent_dir/dir_deep_5/child_dir/file").is_ignore());
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_00/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_00/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_00/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_00/child_dir/file").is_none());
+
+    // 01
+
+    assert!(m("ROOT/parent_dir/dir_deep_01").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_01/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_01/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_01/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_01/child_dir/file").is_none());
+
+    // 02
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_02").is_none());
+
+    assert!(m("ROOT/parent_dir/dir_deep_02/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_02/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_02/child_dir/file").is_none());
+
+    // 03
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_03").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_03/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_03/child_dir/file").is_ignore());
+
+    // 10
+
+    assert!(m("ROOT/parent_dir/dir_deep_10").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_10/file").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_10/child_dir/file").is_none());
+
+    // 11
+
+    assert!(m("ROOT/parent_dir/dir_deep_11").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_11/file").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_11/child_dir/file").is_none());
+
+    // 12
+
+    assert!(m("ROOT/parent_dir/dir_deep_12").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_12/file").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_12/child_dir/file").is_none());
+
+    // 13
+
+    assert!(m("ROOT/parent_dir/dir_deep_13").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_13/file").is_none());
+    assert!(m("ROOT/parent_dir/dir_deep_13/child_dir/file").is_none());
+
+    // 20
+
+    assert!(m("ROOT/parent_dir/dir_deep_20").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_20/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_20/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_20/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_20/child_dir/file").is_none());
+
+    // 21
+
+    assert!(m("ROOT/parent_dir/dir_deep_21").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_21/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_21/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_21/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_21/child_dir/file").is_none());
+
+    // 22
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_22").is_none());
+
+    assert!(m("ROOT/parent_dir/dir_deep_22/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_22/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_22/child_dir/file").is_none());
+
+    // 23
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_23").is_none());
+
+    assert!(m("ROOT/parent_dir/dir_deep_23/file").is_ignore());
+
+    assert!(m("ROOT/parent_dir/dir_deep_23/child_dir/file").is_ignore());
+
+    // 30
+
+    assert!(m("ROOT/parent_dir/dir_deep_30").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_30/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_30/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_30/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_30/child_dir/file").is_none());
+
+    // 31
+
+    assert!(m("ROOT/parent_dir/dir_deep_31").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_31/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_31/file").is_none());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_31/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_31/child_dir/file").is_none());
+
+    // 32
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_32").is_none());
+
+    assert!(m("ROOT/parent_dir/dir_deep_32/file").is_ignore());
+
+    // FIXME: when a dir is ignored, all children are ignored too
+    //assert!(m("ROOT/parent_dir/dir_deep_32/child_dir/file").is_ignore());
+    assert!(m("ROOT/parent_dir/dir_deep_32/child_dir/file").is_none());
+
+    // 33
+
+    // dirs themselves don't matter, so this one is okay
+    assert!(m("ROOT/parent_dir/dir_deep_33").is_none());
+
+    assert!(m("ROOT/parent_dir/dir_deep_33/file").is_ignore());
+
+    assert!(m("ROOT/parent_dir/dir_deep_33/child_dir/file").is_ignore());
 }
